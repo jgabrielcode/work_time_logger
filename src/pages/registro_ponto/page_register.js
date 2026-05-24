5287a81be977f5f3da449ab79444e3ec77b6de84
j_gabrielcode <joao1306gabriel@gmail.com>
2026-05-24 11:59:25 -0300

    Criação login page e funcionalidades em js na register page

--- src/pages/registro_ponto/page_register.js
+++ src/pages/registro_ponto/page_register.js
@@ -1,10 +1,33 @@
 let name = document.querySelector("#name");
+const BT_REGISTER = document.querySelector("#bt_register");
 
+let tempo;
+
+BT_REGISTER.addEventListener("click", () =>{
+    const date = new Date();
+
+    console.log(date.toLocaleTimeString());
+
+    clearTimeout(tempo);
+
+    BT_REGISTER.className = "bi bi-bookmark-check-fill";
+    BT_REGISTER.style.color = "green";
+
+
+    tempo = setTimeout(() => {
+
+       BT_REGISTER.className = "bi bi-play-circle-fill"
+       BT_REGISTER.style.color = "#069bff"
+
+    }, 1000);
+
+});
 
 
 name.textContent = "João Silva";
 
 const hour = document.querySelector(".hour");
+
 const relogio = () => {
     const date = new Date();
     const hora = date.getHours();
